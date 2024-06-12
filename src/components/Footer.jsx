71924c28eda441568a96656e1fffc8c718a512db
sangam5756 <sangammunde3@gmail.com>
2024-06-12 14:47:50 +0530

    Signup-page-Conplete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-slate-200 ">
+      <div className="container mx-auto p-4">
+      <p className="text-center font-bold" title="Github ">Code With Sangam</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer   
+export default Footer;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
-import  {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import signinIcon from "../assest/signin.gif";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  // Password and emial input handling
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleonChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  console.log("Login Data -", data);
+
+  // Handle form when submitted
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <section id="login">
@@ -15,7 +39,7 @@ const Login = () => {
             <img src={signinIcon} alt="login icons" />
           </div>
 
-          <form action="" className="mt-6">
+          <form action="" className="mt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
             <div className="grid">
               <label htmlFor="email">Email :</label>
 
@@ -24,6 +48,8 @@ const Login = () => {
                   type="text"
                   placeholder="Enter Email"
                   name="email"
+                  value={data.email}
+                  onChange={handleonChange}
                   className="w-full outline-none h-full bg-transparent"
                 />
               </div>
@@ -37,26 +63,39 @@ const Login = () => {
                   type={showPassword ? "text" : "password"}
                   placeholder="Enter Password"
                   name="password"
+                  value={data.password}
+                  onChange={handleonChange}
                   className="w-full outline-none h-full bg-transparent"
                 />
-                <div onClick={() => setShowPassword((prev) => (!prev))}>
-                  <span
-                    className="cursor-pointer text-xl"
-                    
-                  >
+                <div onClick={() => setShowPassword((prev) => !prev)}>
+                  <span className="cursor-pointer text-xl">
                     {showPassword ? <FaEyeSlash /> : <FaEye />}
                   </span>
                 </div>
               </div>
+              
 
-
-              <Link to="/forget-password" className=" block w-fit ml-auto hover:text-red-500 hover:underline " >
-              Forget  password</Link>
+              <Link
+                to="/forget-password"
+                className=" block w-fit ml-auto hover:text-red-500 hover:underline "
+              >
+                Forget password ?
+              </Link>
             </div>
 
-            <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">Login</button>
+            <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
+              Login
+            </button>
           </form>
-          <p className="mt-5">Don't have account ? <Link to={"/sign-up"} className=" hover:text-red-700 hover:underline">Sign Up</Link></p>
+          <p className="mt-5">
+            Don't have account ?{" "}
+            <Link
+              to={"/sign-up"}
+              className=" hover:text-red-700 hover:underline text-red-600"
+            >
+              Sign Up
+            </Link>
+          </p>
         </div>
       </div>
     </section>
