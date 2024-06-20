c784ea66ef04ae210a94ae40856680932686c340
sangam5756 <sangammunde3@gmail.com>
2024-06-20 20:12:22 +0530

    hover-image-zoom-done

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import SummaryApi from '../common'
 import { FaStar } from 'react-icons/fa'
@@ -22,6 +22,12 @@ const ProductDetails = () => {
   const productImageListLoading = new Array(4).fill(null);
 
   const [activeImage, setActiveImage] = useState("")
+  const [zoomImageCoordinate, setZoomImageCoordinate] = useState({
+    x: 0,
+    y: 0
+  })
+
+  const [zoom, setZoom] = useState(false)
 
 
   const fetchDetails = async () => {
@@ -34,7 +40,7 @@ const ProductDetails = () => {
     setActiveImage(response?.data?.data?.productImage[0])
     setLoading(false);
   }
-  console.log(data)
+
 
   useEffect(() => {
 
@@ -46,16 +52,57 @@ const ProductDetails = () => {
 
 
   }
+  const handleZoomImage = useCallback((e) => {
+    setZoom(true)
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    console.log("coordinate", left, top, width, height);
+
+    const x = (e.clientX - left) / width
+    const y = (e.clientX - top) / height
+    setZoomImageCoordinate({
+      x, y
+    })
+    
+  }, [zoomImageCoordinate])
+
+  const handleZoomOutImage = () =>{
+    setZoom(false)
+  }
 
   return (
     <div className='container  mx-auto p-4'>
 
       {/* Product Image */}
       <div className=' min-h-[200px] flex flex-col lg:flex-row gap-4 '>
+
+
         <div className='h-96 flex flex-col lg:flex-row-reverse gap-4'>
-          <div className='lg:h-96 lg:w-96 h-[300px] w-[300px]  bg-slate-200'>
-            <img src={activeImage} alt="" className='h-full w-full object-scale-down  mix-blend-multiply' />
-          </div>
+
+          {
+            loading ? (<div className='lg:h-96 lg:w-96 h-[300px] w-[300px] animate-pulse bg-slate-200'>
+            </div>) :
+
+              (<div className='lg:h-96 lg:w-96 h-[300px] w-[300px]  bg-slate-200 ml-5 relative'>
+                <img src={activeImage} alt="" className='h-full w-full object-scale-down  mix-blend-multiply 'onMouseLeave={handleZoomOutImage} onMouseMove={handleZoomImage} />
+
+               {
+                zoom && (
+                  <div className=' absolute bg-slate-200 min-w-[500px] min-h-[400px]  p-2 -right-[510px] overflow-hidden hidden lg:block top-0'>
+                  <div className='h-full min-w-[500px] min-h-[400px] w-full  mix-blend-multiply scale-150'
+                    style={{
+                      backgroundImage: `url(${activeImage})`,
+                      backgroundRepeat: "no-repeat",
+                      backgroundPosition: `${zoomImageCoordinate.x * 100}% ${zoomImageCoordinate.y * 100}%`
+                    }}>
+
+                  </div>
+
+                </div>
+
+                )
+               }
+              </div>)
+          }
 
           <div className='h-full'>
             {
@@ -96,26 +143,58 @@ const ProductDetails = () => {
         </div>
 
         {/* Product details*/}
-        <div className='flex flex-col gap-1'>
-          <p className=' bg-red-200  text-red-600 px-2  rounded-full inline-block w-fit'> {data?.brandName}</p>
-          <h2 className='text-2xl lg:text-4xl font-medium'>{data?.productName}</h2>
-          <p className='capitalize text-slate-400'>{data?.category}</p>
-          <div className='flex  text-red-600 items-center gap-1'>
-            <FaStar/>
-            <FaStar/>
-            <FaStar/>
-            <FaStar/>
-            <FaStarHalf/>
-          </div>
-          <div className='flex items-center gap-2 text-2xl lg:text-3xl my-2 '>
-            <p className='text-red-600'>{DisplayInrCurrency(data?.sellingPrice)}</p>
-            <p className='line-through text-slate-400 '>{DisplayInrCurrency(data?.price)}</p>
-          </div>
+        {
+          loading ? (
+
+
+            <div className='flex flex-col w-full gap-5'>
+              <p className=' bg-slate-200  px-7  rounded-full inline-block w-fit p-5  animate-pulse'> </p>
+              <h2 className='text-2xl lg:text-4xl font-medium p-5 rounded w-21 bg-slate-200'></h2>
+              <p className='capitalize text-slate-400  bg-slate-200 p-5'></p>
+              <div className='flex  text-slate-200 animate-pulse items-center gap-1 bg-slate-200 p-5'>
+
+              </div>
+              <div className='flex items-center gap-3 text-2xl lg:text-3xl my-2 w-full '>
+                <p className='text-red-600  bg-slate-200 p-3 w-full'></p>
+                <p className='line-through  bg-slate-200 p-3 text-slate-400 w-full'></p>
+              </div>
+
+              <div className='flex  justify-between   gap-6 my-2'>
+                <button className=' border-2  bg-slate-200 p-2 rounded px-3   animate-pulse py-4 min-w-[400px] '></button>
+                <button className=' border-2  rounded px-3 py-4 bg-slate-200  animate-pulse  min-w-[400px] '></button>
+
+              </div>
+
+              <div>
+                <p className=' text-slate-600 font-medium my-1 bg-slate-200 animate-pulse p-2'></p>
+                <p className=' w-fit mt-1'></p>
+              </div>
+
+            </div>
+
+
+
+          ) : (
+            <div className='flex flex-col gap-1'>
+              <p className=' bg-red-200  text-red-600 px-2  rounded-full inline-block w-fit'> {data?.brandName}</p>
+              <h2 className='text-2xl lg:text-4xl font-medium'>{data?.productName}</h2>
+              <p className='capitalize text-slate-400'>{data?.category}</p>
+              <div className='flex  text-red-600 items-center gap-1'>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStarHalf />
+              </div>
+              <div className='flex items-center gap-2 text-2xl lg:text-3xl my-2 '>
+                <p className='text-red-600'>{DisplayInrCurrency(data?.sellingPrice)}</p>
+                <p className='line-through text-slate-400 '>{DisplayInrCurrency(data?.price)}</p>
+              </div>
 
               <div className='flex items-center gap-3 my-2'>
                 <button className=' border-2  border-red-600 text-red-600 font-medium  hover:bg-red-600 hover:text-white duration-200 rounded px-3 py-1 min-w-[120px] '>Buy</button>
                 <button className=' border-2  border-red-600 text-white   font-medium  bg-red-600         hover:text-red  hover:bg-white hover:text-red-600 duration-200 rounded px-3 py-1 min-w-[120px] '>Add to Cart</button>
-              
+
               </div>
 
               <div>
@@ -123,7 +202,9 @@ const ProductDetails = () => {
                 <p>{data?.description}</p>
               </div>
 
-        </div>
+            </div>
+          )
+        }
       </div>
 
 
